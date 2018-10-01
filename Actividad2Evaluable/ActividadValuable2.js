256628ef5ffb68f249c8fa0c056fc91045141908
WINMAC11\2daw <jobanyuls@gmail.com>
2018-10-01 13:31:29 +0200

    Actividad 2

---
+++ Actividad2Evaluable/ActividadValuable2.js
@@ -0,0 +1,12 @@
+class articuloscientificos {
+    constructor(title, author, numpaginas, anyodepublicacion, nummenciones){
+        this.title = title;
+        this.author = author;
+        this.numpaginas = numpaginas;
+        this.anyodepublicacion = anyodepublicacion;
+        this.nummenciones = nummenciones;
+    }
+    
+
+
+}

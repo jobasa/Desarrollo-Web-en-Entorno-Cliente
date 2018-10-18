43f476a83439064db42a62803885acdcbbd4640e
WINMAC11\2daw <jobanyuls@gmail.com>
2018-10-18 09:53:25 +0200

    Modificar apellidos

--- Actividad2Evaluable/ActividadValuable2.js
+++ Actividad2Evaluable/ActividadValuable2.js
@@ -70,7 +70,7 @@
             console.log('1) Autores');
             console.log('2) Articulos cientificos');
             console.log('3) Patentes cientificas');
-            let opcion = readline.questionInt('Por favor seleccione una de estas opciones: ');
+            let opcion = readline.questionInt('Por favor selecciona una de estas opciones: ');
             if(opcion === 1){
                 let nombre = readline.question('Por favor introduce un nombre: ');
                 let apellidos = readline.question('Por favir introduce los apellidos: ');
@@ -82,46 +82,97 @@
                 console.log('Autor registrado con exito!');
             }
             if(opcion === 2){
-                console.log('¿Que tipo de articulo desea registrar');
+                console.log('¿Que tipo de articulo quieres registrar');
                 console.log('1) Articulo de revista');
                 console.log('2) Articulo de conferencia');
-                let opcion = readline.questionInt('Por favor seleccione una de estas opciones: ');
+                let opcion = readline.questionInt('Por favor selecciona una de estas opciones: ');
                 
                 if(opcion === 1){
+                    //Articulo revistas
                     let titulo = readline.question('Por favor introduce un titulo: ');
                     let numAutores = readline.question('¿Cuantos autores tiene?: ');
                     let autor = [];
                     for(let i = 0; i < numAutores; i++){
                         autor[i] = readline.question('Por favor introduce un autor: ');
                 }
-                let numPaginas = readline.question('Por favor introduce una numero de paginas: ');
+                let numPaginas = readline.question('Por favor introduce un numero de paginas: ');
                 let anyoPublicacion = readline.questionInt('Por favor introduce un anyo de publicacion: ');
                 let numMenciones = readline.question('Por favor introduce un numero de menciones: ');
                 let editorial = readline.question('Por favor introduce una editorial: ');
-                let factorImpacto = readline.questionFloat('Por favor introduce factor de impacto: ');
-                let newArticuloRevista = new articulosRevista(titulo, autor,numPaginas,anyoPublicacion, editorial, numMenciones,factorImpacto);
+                let factorImpact = readline.questionFloat('Por favor introduce un factor de impacto: ');
+                let newArticuloRevista = new articulosRevista(titulo, autor,numPaginas,anyoPublicacion, editorial, numMenciones,factorImpact);
                 listaRevista.push(newArticuloRevista);
                 console.log(listaRevista);
-                console.log('Registro se ha realizado con exito');
+                console.log('Registro realizado con exito!');
                 
             }
-        }else if(opcion===2){
-            //Dar de baja un libro
-            let titulo=readline.question('Por favor introduce un titulo: ');
-            for(let i=0; i<listaLibro.length;i++){
-                let libro = listaLibro[i];
-                if (libro.titulo===titulo) {
-                    listaLibro.splice(i);
-                    encontrado = true;
-                    break;
+                if(opcion === 2){
+                    //Articulo de conferencia
+                    let titulo = readline.question('Por favor introduce un titulo: ');
+                    let numAutores = readline.question('¿Cuantos autores tiene?: ');
+                    let autor = [];
+                    for(let i = 0; i < numAutores; i++){
+                        autor[i] = readline.question('Por favor introduce un autor: ');
                 }
-            }
-            if(encontrado){
-                console.log('Libro encontrado y borrado del sistema');
-                console.log(listaLibros);
-            }
-        }else if(opcion===3){
+                let numPaginas = readline.question('Por favor introduce un numero de paginas: ');
+                let anyoPublicacion = readline.questionInt('Por favor introduce un anyo de publicacion: ');
+                let numMenciones = readline.question('Por favor introduce un numero de menciones: ');
+                let nomConferencia = readline.question('Por favor introduce el nombre de la conferencia: ');
+                let lugarCelebracion = readline.question('Por favor introduce el lugar de celebracion: ');
+                let newArticuloConferencia = new articulosConferencia(titulo,autor,numPaginas,anyoPublicacion,numMenciones,nomConferencia,lugarCelebracion);
+                listaConferencia.push(newArticuloConferencia);
+                console.log(listaConferencia);
+                console.log('Registro realizado con exito');
+                }
+        }else if(opcion===2){
+            //Dar de baja
 
+        }else if(opcion===3){
+            //Modificar
+            console.log('¿Que quieres modificar?\n');
+            console.log('1) Autores');
+            console.log('2) Articulos cientificos');
+            console.log('3) Patentes cientificas');
+            let opcion = readline.questionInt('Por favor selecciona una de estas opciones: ');
+
+            if(opcion===1){
+                if(opcion === 1){
+                    console.log('¿Que quieres modificar?\n');
+                    console.log('1) Nombre');
+                    console.log('2) Apellidos');
+                    let opcion = readline.questionInt('Por favor seleccione una de estas opciones: ');
+                    
+                    //Nombre
+                    if(opcion===1){
+                        for(let i = 0; i < listaAutor.length; i++){
+                            let autor = listaAutor[i];
+                            if(autor.nombre === opcion){
+                                let nuevoNombre = readline.question('Introduce un nuevo nombre: ');
+                                autor.nombre = nuevoNombre;
+                                console.log(listaAutor);
+                                console.log('Nombre modificado con exito!');
+                                break;
+                            } else {
+                                console.log('No se ha encontrado el autor');
+                                salir = true;
+                            }
+                        }
+                    }
+
+                    //Apellido
+                    if(opcion===2){
+                        for(let i = 0; i < listaAutor.length; i++){
+                            let autor = listaAutor[i];
+                            if(autor.apellidos === opcion){
+                                let nuevoApellido = readline.question('Introduce un nuevo apellido: ');
+                            }
+                        }
+                    }
+
+                }
+
+            }
+    
 
         }else if(opcion===4){
             salir=true;

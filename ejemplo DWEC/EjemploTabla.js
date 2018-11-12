e377bfbc6e8b2c1e37804984ecc45c079a79b36f
WINMAC11\2daw <jobanyuls@gmail.com>
2018-11-12 11:49:12 +0100

    Ejemplo DOM

--- ejemplo DWEC/EjemploTabla.js
+++ ejemplo DWEC/EjemploTabla.js
@@ -1,4 +1,14 @@
-console.log(document.body.children[0]);
-let tabla = document.body.children[0];
-let celdas = ;
+let table = document.body.firstElementChild;
+
+let tableBody = table.tBodies[0];
+
+let rows = tableBody.rows;
+
+for(let row of rows) {
+    let cells = row.cells;
+    let salary = cells[2].textContent;
+    if (parseFloat(salary)<0) {
+        cells[2].style = 'background-color: red;';
+    }
+}
 

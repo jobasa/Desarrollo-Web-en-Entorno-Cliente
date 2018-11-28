d582d3b1a0bf600aba94befac0904254bcefb242
WINMAC11\2daw <jobanyuls@gmail.com>
2018-11-28 12:58:37 +0100

    actividad de clase

---
+++ project-html-website-master/script.js
@@ -0,0 +1,10 @@
+/*Bájate el proyecto Web Azure de ejemplo de FloridaOberta 
+y haz un script de JS que saque todos los links de la página web.
+ Se deberá extraer los hostnames únicos a los que enlaza la página.*/
+
+let link = document.getElementsByTagName("a");
+console.log(link);
+
+for (links of link)
+console.log(links.hostname);
+

c30f6da8e56f6c0f7e4a53be798cc4dc5506be4d
WINMAC11\2daw <jobanyuls@gmail.com>
2018-12-05 13:10:31 +0100

    en proceso

--- ACTIVIDAD PRÁCTICA 4 PUZZLES/puzzle.js
+++ ACTIVIDAD PRÁCTICA 4 PUZZLES/puzzle.js
@@ -1,15 +1,44 @@
-let pregunta = window.prompt("Elige el numero de piezas del puzzle:")
+/*let pregunta = window.prompt("Elige el numero de piezas del puzzle:")
 console.log(pregunta);
 
 let raiz_cuadrada= Math.sqrt(pregunta);
 let comprobar = Number.isInteger(raiz_cuadrada);
-console.log(comprobar);
+console.log(comprobar);*/
 
 
 
 function getNumberPiecesFromUser() {
-    //while () {
-        
-    //}
+    
+    let pregunta;
+    let raiz_cuadrada= Math.sqrt(17);
+
+    while (!Number.isInteger(raiz_cuadrada)) {
+        pregunta = window.prompt("Elige el numero de piezas del puzzle:")
+        raiz_cuadrada= Math.sqrt(pregunta);
+
+         
+    }
+
+    return parseInt(pregunta);
+    
+    
+    
     
 }
+
+
+function  getMaxScore(num_piezas) {
+ let resultado = num_piezas * 2;
+ return resultado;
+}
+
+function getScore() {
+    
+}
+
+let texto = document.
+console.log(texto);
+
+getNumberPiecesFromUser();
+getMaxScore(2);
+

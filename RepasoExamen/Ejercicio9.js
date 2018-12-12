6ebb144519f2bdec30838029f715013593304414
DESKTOP-A5CERV0\Joan <jobanyuls@gmail.com>
2018-12-12 20:45:05 +0100

    .

---
+++ RepasoExamen/Ejercicio9.js
@@ -0,0 +1,12 @@
+/*Crea una función llamada circumferenceArea que tome como parámetro 
+el radio de la circunferencia radius y devuelva el área de la circunferencia.*/
+
+
+radio = 15;
+function circumferenceArea(radio) {
+    areaCircum = 3.14*radio**2
+    console.log(areaCircum);
+}
+
+console.log(circumferenceArea(radio));
+//Duda: porque sale el undefined

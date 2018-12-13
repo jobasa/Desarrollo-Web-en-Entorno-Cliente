c7f533186706e5df8f2a91aa7332bddc6a3621d0
WINMAC11\2daw <jobanyuls@gmail.com>
2018-12-13 09:52:51 +0100

    en proceso

--- ACTIVIDAD PRÁCTICA 4 PUZZLES/puzzle.js
+++ ACTIVIDAD PRÁCTICA 4 PUZZLES/puzzle.js
@@ -8,44 +8,53 @@ console.log(comprobar);*/
 
 
 function getNumberPiecesFromUser() {
-    
+
     let pregunta;
-    let raiz_cuadrada= Math.sqrt(17);
+    let raiz_cuadrada = Math.sqrt(17);
 
     while (!Number.isInteger(raiz_cuadrada)) {
         pregunta = window.prompt("Elige el numero de piezas del puzzle:")
-        raiz_cuadrada= Math.sqrt(pregunta);
+        raiz_cuadrada = Math.sqrt(pregunta);
 
-         
+
     }
 
     return parseInt(pregunta);
-    
-    
-    
-    
+
+
+
+
 }
 
 let num_piezas = 2;
-function  getMaxScore(num_piezas) {
- let resultado = num_piezas * 2;
- return resultado;
+
+function getMaxScore(num_piezas) {
+    let resultado = num_piezas * 2;
+    return resultado;
 }
 
 function getScore() {
     let score = document.getElementById("score").textContent;
     score = score.split('Score:');
-    parseInt(score[1]);
+    score = parseInt(score[1]);
     return score;
-    
+
 }
 
-function updateScore(score) {
+function updateScore(newScore) {
+    let viejoScore = document.getElementById("score").textContent = "Score: " + newScore;
+    return viejoScore;
+
+}
+
+function decreaseScore(numero) {
+    let puntuacion = getScore();
+    let decrementoScore = puntuacion - numero;
+    updateScore(decrementoScore);
     
 }
 
 
 getNumberPiecesFromUser();
 console.log(getScore());
-
-
+console.log(decreaseScore(1));
